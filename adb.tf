6b121e97e6eb128da950f2c1dd11eefa4cbdf19e
facundocayon <facundocayon@gmail.com>
2024-06-06 18:09:43 -0300

    Update adb.tf

--- adb.tf
+++ adb.tf
@@ -1,11 +1,11 @@
 module "oci-adb" {
   source                    = "github.com/fc-terraform-code/tfc-oci-adb-module"
-  db_name                   = "trondataware2"
+  db_name                   = "trontransac"
   admin_password            = var.admin_password
   compartment_id            = var.compartment_ocid
-  db_workload               = "DW" # Autonomous data warehouse (ADW)
+  db_workload               = "OLTP" #--Transaction Processing (Transactional workloads)
   license_model             = "LICENSE_INCLUDED"
-  db_version                = "21c"
+  db_version                = "23ai"
   is_free_tier              = "true"  
   tenancy_ocid              = var.tenancy_ocid
   user_ocid                 = var.user_ocid
@@ -14,11 +14,11 @@ module "oci-adb" {
   region                    = var.region
   fingerprint               = var.fingerprint
   whitelisted_ips           = ["0.0.0.0/0"]
-  tag_namespace_name        = "trondataware2"
+  tag_namespace_name        = "trontransac"
   identity_tag_name         = "iac"
   freeform_tags =  {
-    Environment = "fc-dev"
-    Department = "fc-ops"
+    Environment = "dev"
+    Department = "ops"
   }
   defined_tags = {
     "trondataware.iac" = "terraform"
